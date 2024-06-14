f80aedcf7c6639753ed814968dcc04d905d7495d
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2024-06-14 13:05:41 +0530

    Adds randomize utility and eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
+		jquery: true,
 	},
 	extends: 'airbnb-base',
+	plugins: ['jquery'],
 	overrides: [
 		{
 			env: {
@@ -21,7 +23,21 @@ module.exports = {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
-	rules: {},
+	rules: {
+		'indent': ['error', 4],
+
+		// Move the invocation inside the IIFE
+		'wrap-iife': ['error', 'inside'],
+
+		// Allow unnamed functions where necessary
+		'func-names': 'off', 
+		
+		// Allow assignment to properties of function parameters
+		'no-param-reassign': 'off',
+
+		// Prefer arrow functions where possible
+		'prefer-arrow-callback': 'error',
+	},
 	settings: {
 		"import/resolver": {
 			"babel-module": {}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '@utils';
+
+$(document).ready(() => {
+    // onload randomise
+    $('div.dfc-realtors div.et_pb_row div.dfc-realtor')
+        .randomize('div.dfc-realtors div.et_pb_row');
+});
