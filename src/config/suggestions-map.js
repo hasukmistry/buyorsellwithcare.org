594daa2ddff1e1a00e3c4e7fe0b91ac4cabdd1f7
nthiele7332 <nick.thiele@gmail.com>
2024-12-03 19:59:28 -0700

    Update suggestions-map.js

--- src/config/suggestions-map.js
+++ src/config/suggestions-map.js
@@ -4,7 +4,7 @@ const suggestionsMap = {
     ['Davis County']: ['Layton', 'Bountiful', 'Centerville', 'Clearfield', 'Farmington', 'Kaysville', 'North Salt Lake', 'Syracuse', 'Woods Cross'],
     ['Salt Lake County']: ['Draper', 'Riverton', 'West Jordan', 'Herriman', 'Salt Lake City', 'Bingham Canyon', 'South Jordan', 'Magna', 'Midvale', 'Sandy', 'West Valley City'],
     ['Utah County']: ['Alpine', 'American Fork', 'Elk Ridge', 'Spanish Fork', 'Lehi', 'Springville', 'Saratoga Springs', 'Eagle Mountain', 'Pleasant Grove', 'Woodland Hills', 'Lindon', 'Vineyard', 'Highland', 'Mapleton', 'Salem', 'Payson', 'Provo', 'Orem', 'Santaquin', 'Elberta', 'Goshen', 'Cedar Valley'],
-    ['Wasatch County']: ['Heber City', 'Midway', 'Wallsburg'],
+    ['Wasatch County']: ['Heber City', 'Midway', 'Wallsburger'],
     ['Washington County']: ['Central', 'Dammeron Valley', 'Enterprise', 'Gunlock', 'Hildale', 'Hurricane', 'Ivins', 'La Verkin', 'Leeds', 'New Harmony', 'Pine Valley', 'Rockville', 'Saint George', 'Santa Clara', 'Springdale', 'Toquerville', 'Veyo', 'Virgin', 'Washington'],
     ['Weber County']: ['Eden', 'Hooper', 'Huntsville', 'Ogden', 'Roy'],
 };
