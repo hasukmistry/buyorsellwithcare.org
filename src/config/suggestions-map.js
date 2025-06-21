7ae641a575d3ac436762146fbc9fb951c33f2e19
nthiele7332 <nick.thiele@gmail.com>
2025-06-20 20:33:55 -0400

    Update suggestions-map.js

--- src/config/suggestions-map.js
+++ src/config/suggestions-map.js
@@ -1,6 +1,6 @@
 const suggestionsMap = {
     ['Fairfield County']: ['Winnsboro', 'Winnsboro Mills'],
-    ['Greenville County']: ['Greenville', 'Greer', 'Mauldin', 'Simppsonville', 'Taylors', 'Wade Hampton', 'Five Forks', 'Gantt', 'Fountain Inn', 'Parker', 'Travelers Rest'],
+    ['Greenville County']: ['Greenville', 'Greer', 'Mauldin', 'Simpsonville', 'Taylors', 'Wade Hampton', 'Five Forks', 'Gantt', 'Fountain Inn', 'Parker', 'Travelers Rest'],
     ['Kershaw County']: ['Lugoff', 'Camden', 'East Camden', 'Elgin'],
     ['Lexington County']: ['Cayce', 'West Columbia', 'Chapin', 'Lexington', 'Batesburg-Leesville', 'Gaston', 'Gilbert', 'Columbia', 'Seven Oaks', 'St. Andrews', 'Red Bank', 'White Knoll'],
     ['Newberry County']: ['Newberry', 'Whitmire', 'Prosperity'],
